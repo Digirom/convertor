8f5bf747396b981f0df4b365f4c886de1427b7d5
Digirom <64632113+Digirom@users.noreply.github.com>
2022-05-05 20:06:23 +0500

    Initial commit

---
+++ app/pages/PageEnergy.vue
@@ -0,0 +1,8 @@
+<template>
+    <Page>
+      <ActionBar title="Энергия"/>
+      <StackLayout verticalAlignment="top">
+        <button @tap="$navigator.back()" text="Назад" android:marginTop="20" />
+      </StackLayout>
+    </Page>
+</template>
---
+++ app/pages/PageWeight.vue
@@ -0,0 +1,8 @@
+<template>
+    <Page>
+      <ActionBar title="Вес"/>
+      <StackLayout verticalAlignment="top">
+        <button @tap="$navigator.back()" text="Назад" android:marginTop="20" />
+      </StackLayout>
+    </Page>
+</template>
